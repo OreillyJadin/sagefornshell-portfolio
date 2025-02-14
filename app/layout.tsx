0235c185af9cd57e2880b92e4fbaa7f88f6ec46e
OreillyJadin <oreillyjadin24@gmail.com>
2025-02-14 12:49:46 -0600

    Search engine changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,21 @@ export const metadata: Metadata = {
   title: "Sage Fornshell",
   description: "Sales Professional @ Kansas State University",
   generator: "Next.js",
+  applicationName: "Sage Fornshell Portfolio",
+  referrer: "origin-when-cross-origin",
   keywords: [
     "Sage Fornshell",
     "Portfolio",
-    "Sales",
+    "Kansas State University",
     "Fornshell",
     "Bunzl",
     "Bunzl Sage",
     "Bunzl Fornshell",
-    "Buzl Sage Fornshell",
+    "Bunzl Sage Fornshell",
   ], // Add keywords for SEO
   authors: [{ name: "Sage Fornshell" }], // Add author information
   openGraph: {
-    title: "Sage Fornshell - Portfolio",
+    title: "Sage Fornshell",
     description: "Sales Professional @ Kansas State University",
     url: "https://sagefornshell.com",
     siteName: "Sage Fornshell",
@@ -26,10 +28,24 @@ export const metadata: Metadata = {
         url: "/Fornshell_Sage_Headshot-2.jpg", // Add an Open Graph image public/Fornshell_Sage_Headshot-2.jpg
         width: 1200,
         height: 630,
+        alt: "Sage Fornshell - Sales Professional & Business Consultant",
       },
     ],
     locale: "en_US",
     type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 
@@ -44,33 +60,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-/*
-export const metadata: Metadata = {
-  title: 'Sage Fornshell - Portfolio',
-  description: 'Sales Professional & Business Consultant',
-  generator: 'Next.js',
-  keywords: ['Sage Fornshell', 'Portfolio', 'Sales', 'Business Consultant'], // Add keywords for SEO
-  authors: [{ name: 'Sage Fornshell' }], // Add author information
-  openGraph: {
-    title: 'Sage Fornshell - Portfolio',
-    description: 'Sales Professional & Business Consultant',
-    url: 'https://sagefornshell.com', // Replace with your domain
-    siteName: 'Sage Fornshell',
-    images: [
-      {
-        url: 'https://sagefornshell.com/og-image.jpg', // Add an Open Graph image
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Sage Fornshell - Portfolio',
-    description: 'Sales Professional & Business Consultant',
-    images: ['https://sagefornshell.com/twitter-image.jpg'], // Add a Twitter card image
-  },
-};*/
